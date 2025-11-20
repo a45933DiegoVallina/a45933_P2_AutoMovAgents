27390e6593d1336604cdbba860a28095c23af688
Diego Vallina <a45933@alunos.ipb.pt>
2025-11-20 19:07:21 +0000

    20/11/2025
    Pursuit (done)

--- Assets/Bot.cs
+++ Assets/Bot.cs
@@ -26,10 +26,28 @@ public class Bot : MonoBehaviour
         agente.SetDestination(this.transform.position - fleeVector);
     }
 
+    void Pursue()
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float relativeHeading = Vector3.Angle(this.transform.forward, this.transform.TransformVector(target.transform.forward));
+        float toTarget = Vector3.Angle(this.transform.forward, this.transform.TransformVector(targetDir));
+
+        if ((toTarget > 90 && relativeHeading < 20) || target.GetComponent<Drive>().currentSpeed < 0.01f)
+        {
+            Seek(target.transform.position);
+            return;
+        }
+        
+        float lookAhead = targetDir.magnitude / (agente.speed + target.GetComponent<Drive>().currentSpeed);
+        Seek(target.transform.position + target.transform.forward * lookAhead);
+    }
+
     // Update is called once per frame
-    void Update()
+    void Update()  //Estas 3 linhas de 
     {
         //Seek(target.transform.position);
-        Flee(target.transform.position);
+        //Flee(target.transform.position);
+        Pursue();
     }
 }
